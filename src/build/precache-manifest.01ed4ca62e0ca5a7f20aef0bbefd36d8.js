fc1364e778e60af19d35dcf08945bd6e568765bc
DamilolaAjayi <ajayidamey@gmail.com>
2020-07-09 20:18:32 +0100

    First commit

---
+++ src/build/precache-manifest.01ed4ca62e0ca5a7f20aef0bbefd36d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4954a38bb55ad92bdc7daedab596f5e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91f5d0d07cf72c7d7233",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9216b433b0dfd01099a6",
+    "url": "/static/js/2.cc7f2dad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cc7f2dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f5d0d07cf72c7d7233",
+    "url": "/static/js/main.43081e46.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
